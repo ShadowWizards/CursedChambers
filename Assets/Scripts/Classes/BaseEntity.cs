a48bfd86fe2d80cd2b31d2c9c814b1b93e42077c
Yuu-to-kun <yuutokunowo@gmail.com>
2023-11-21 21:01:21 +0100

    Added Base Entity + Player Class

---
+++ Assets/Scripts/Classes/BaseEntity.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEntity
+{
+    public double Hp;
+    public double Str;
+    public double Def;
+    public bool Friendly;
+}
---
+++ Assets/Scripts/Classes/PlayerClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Classes
+{
+    public class PlayerClass : BaseEntity
+    {
+        public int inventorySpace;
+        public string[] buffs;
+        public string[] debuffs;
+    }
+}
