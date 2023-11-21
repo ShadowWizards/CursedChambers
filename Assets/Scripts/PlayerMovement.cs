21e05fcf6264cc75dbe6361b5843a9000bc8d963
EvilTerry <Evilterixyoutub@gmail.com>
2023-11-21 20:01:57 +0100

    add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Movement
+    public float moveSpeed;
+    Rigidbody2D rb;
+    Vector2 moveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputManagment();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputManagment()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+    }
+}
