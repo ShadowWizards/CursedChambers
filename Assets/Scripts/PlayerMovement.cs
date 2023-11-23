462e07ed3d54858d5aa234931e9c060d728bc34b
Yuu-to-kun <yuutokunowo@gmail.com>
2023-11-23 06:32:56 +0100

    Player Movement
    Rewrote player movement script as well as the whole player gameobject  remade animations and reassigned the animations inside the Animator window

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,42 +1,89 @@
-using System.Collections;
-using System.Collections.Generic;
+using Assets.Scripts.Classes;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Movement
-    public float moveSpeed;
-    Rigidbody2D rb;
-    [HideInInspector]
-    public Vector2 moveDir;
+    private float speed;
+    private Rigidbody2D playerRigidBody;
+    private PlayerClass player;
+    private Vector2 moveInput;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        //Get the animator of the player
+        animator = GetComponent<Animator>();
+
+        //Get the Body of the player
+        playerRigidBody = GetComponent<Rigidbody2D>();
+
+        //Get the spriteRenderer of the player
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //Set up player speed
+        player = GetComponent<PlayerClass>();
+        player.Speed = 0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        InputManagement();
+        
+        speed = GetComponent<PlayerClass>().Speed;
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+        animator.SetFloat("Speed",speed);
+        animator.SetFloat("Horizontal", moveInput.x);
+        animator.SetFloat("Vertical", moveInput.y);
+
+
+        if (moveInput.x == 0 && moveInput.y == 0)
+        {
+            player.Speed = 0;
+        }
+        else
+        {
+            player.Speed = 1;
+        }
+        
+        if (moveInput.x < 0.1)
+        {
+            transform.localRotation = Quaternion.Euler(0,0,0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+
+        }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        Move();
-    }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
-    void InputManagement()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        bool isMovingHorizontal = Mathf.Abs(horizontal) > 0.5f;
+        bool isMovingVertical = Mathf.Abs(vertical) > 0.5f;
 
-        moveDir = new Vector2(moveX, moveY).normalized;
-    }
 
-    void Move()
-    {
-        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+        if (isMovingVertical)
+        {
+            animator.SetFloat("Vertical", moveInput.y);
+            playerRigidBody.velocity = new Vector2(0, vertical * speed);
+        }
+        else if (isMovingHorizontal)
+        {
+            animator.SetFloat("Horizontal", moveInput.x);
+            playerRigidBody.velocity = new Vector2(horizontal * speed, 0);
+        }
+        else
+        {
+            playerRigidBody.velocity = Vector2.zero;
+        }
+        
     }
 }
