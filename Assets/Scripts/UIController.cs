4172d8079365e40f92a06d3f5f97f53834c63630
jpos787 <janek.pospiech@gmail.com>
2024-01-10 12:33:53 +0100

    audio men/score men

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Slider _musicSlider,_sfxSlider;
+    public void ToggleMusic(){
+        AudioMenager.Instance.ToggleMusic();
+    }
+   public void ToggleSFX(){
+    AudioMenager.Instance.ToggleSFX();
+   }
+   public void MusicVolume(){
+    AudioMenager.Instance.MusicVolume(_musicSlider.value);
+   }
+   public void SFXVolume(){
+    AudioMenager.Instance.MusicVolume(_sfxSlider.value);
+   }
+}
